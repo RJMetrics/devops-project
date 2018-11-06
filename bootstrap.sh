c89f69a9dcc688e3ecfaf582ada0803417b833f4
Subodh Brahmi <subodh_brahmi@yahoo.co.in>
2018-11-06 09:40:03 -0600

    added apache and php config and page

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,41 +23,70 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBRPASSWD"
 
 
-# Installing packages
+#Installing packages
 apt-get install -y mysql-server mysql-client   
-# Allow External Connections on your MySQL Service
-sudo sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+#Allow External Connections on your MySQL Service
+sed -i -e 's/bind-addres/#bind-address/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sed -i -e 's/skip-external-locking/#skip-external-locking/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 mysql -u root -p$DBRPASSWD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY $DBRPASSWD; FLUSH privileges;"
 
 echo -e "\n ************ Setting up our MySQL user and db ****************** \n"
 
-# Import SQL data, if it is sitting in /vagrant dir
+#Import SQL data, if it is sitting in /vagrant dir
 if [ ! -z `ls /vagrant/*.sql` ]; then	
-	# Set variable to the basename of the file, minus '.sql'
+	#Set variable to the basename of the file, minus '.sql'
 	DBNAME=`ls /vagrant/*.sql | cut -d '/' -f 3 | sed s/.sql//`
 	
-	# Create a database with that name
+	#Create a database with that name
 	echo -e "Creating Database --- " $DBNAME
 	mysqladmin -u root -p$DBRPASSWD create $DBNAME
 
 
 	
-	# Import the SQL into new database
+	#Import the SQL into new database
 	`mysql -u root -p$DBRPASSWD $DBNAME < /vagrant/$DBNAME.sql`
 
 	echo -e "*************** Data Inserted Successfully !!! ********************"
 
-	# Create a new user with same name as new db, with password 'pass'
+	#Create a new user with same name as new db, with password 'pass'
 	echo -e "**** Creating new db user *****"
 	mysql -u root -p$DBRPASSWD -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'%' IDENTIFIED BY $DBPASSWD WITH GRANT OPTION; FLUSH PRIVILEGES;"
 fi
 
-sudo service mysql restart
+#Restart mysql
+service mysql restart
 
 
 
 
 #Install apache 2
-echo Installing apache2
+echo -e  "****** Installing apache2 ******"
 apt-get install -y apache2
+
+# Enable Apache mods
+a2enmod rewrite
+
+
+
+# Add Ondrej PPA Repo, required for php7
+apt-add-repository ppa:ondrej/php
+apt-get update
+
+
+#Install PHP
+echo -e "******** Installing PHP7.2 ***************"
+apt-get install -y php7.2
+
+#PHP Apache Mod
+apt-get install -y libapache2-mod-php7.2
+
+#Restart Apache
+service apache2 restart
+
+#Installing PHP Mods and mysql lib
+apt-get install -y php7.2-common php7.2-mcrypt php7.2-zip php7.2-mysql
+
+
+
+#Restart Apache
+service apache2 restart
---
+++ js/phpinfo.php
@@ -0,0 +1,18 @@
+/**
+ * phpinfo.php page should be at 
+ *  http://localhost:8001/phpinfo.php
+ */
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title> PHP INFO PAGE </title>
+
+</head>
+<body>
+	<h1> PHP Installation and configuration info</h1>
+	<?php phpinfo(); ?>
+
+</body>
+
+</html>
