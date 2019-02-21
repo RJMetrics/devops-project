aa6c6d1e283dba8fb1a26ad19d1e2e9bbc0df674
usunnapu <umesh.sunnapu@gmail.com>
2019-02-21 10:50:32 -0600

    added conditions to recipe

--- cookbooks/base/recipes/default.rb
+++ cookbooks/base/recipes/default.rb
@@ -1,11 +1,23 @@
 execute 'base_setup' do
 	command "add-apt-repository ppa:ondrej/php && apt-get update"
+	not_if 'apt-cache policy | grep ondrej'
 end
 
 package 'python-pip' do
 	action :install
 end
 
-execute 'python_packages' do
-	command "pip install mysql-connector-python && pip install pexpect && pip install requests"
+execute 'python-requests' do
+	command "pip install requests"
+	not_if 'pip list | grep requests'
+end
+
+execute 'python-mysql-client' do
+	command "pip install mysql-connector-python"
+	not_if 'pip list | grep mysql-connector-python'
+end
+
+execute 'python-pexpect' do
+	command "pip install pexpect"
+	not_if 'pip list | grep pexpect'
 end
